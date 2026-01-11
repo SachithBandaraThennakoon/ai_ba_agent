db3fa1215cc7c727fdac700e96cdbba113c70319
SachithShilshan <sachith.edu6021@gmail.com>
2026-01-11 10:35:34 +0530

    jan 11

--- api/wsgi.py
+++
@@ -1,6 +0,0 @@
-from fastapi import FastAPI
-from fastapi.middleware.wsgi import WSGIMiddleware
-from api.main import app as fastapi_app
-
-# Create a WSGI-compatible app
-app = WSGIMiddleware(fastapi_app)
